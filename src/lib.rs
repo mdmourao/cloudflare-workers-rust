dc55e0fb22a0abfd6b6098906777c5165460f65c
mdmourao <martimmourao@gmail.com>
2026-01-20 17:05:46 +0000

    first blood

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+use worker::*;
+
+#[event(fetch)]
+async fn fetch(
+    _req: HttpRequest,
+    _env: Env,
+    _ctx: Context,
+) -> Result<HttpResponse> {
+    let body = Body::from_stream(futures_util::stream::once(async {
+        Ok::<_, worker::Error>("Hello World".as_bytes().to_vec())
+    }))?;
+    
+    http::Response::builder()
+        .status(200)
+        .body(body)
+        .map_err(|e| Error::from(e.to_string()))
+}
